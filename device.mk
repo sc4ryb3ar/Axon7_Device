e2decd7f1ef667928d00ebeabb1b6952183c1b82
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:41:25 -0700

    axon7: Add DRM HAL impl
    Change-Id: I3e4bbe1731a671ee11b8b1882b379e7b36fb2f18
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- device.mk
+++ device.mk
@@ -180,6 +180,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ZteDoze
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # Fingerprintd
 PRODUCT_PACKAGES += \
     fingerprintd
