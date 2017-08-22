4f2228d52d9268a21577f096dccb703a7c323ef8
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:44:08 -0700

    axon7: Add vibrator HAL impl
    Change-Id: I4501b7aaf957a87205f21614ba8b23837794b1c4
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- device.mk
+++ device.mk
@@ -323,6 +323,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \
