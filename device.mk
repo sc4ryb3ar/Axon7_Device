f931141847edc793bd397978d886cb09cda3cc83
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:42:37 -0700

    axon7: Add sensors HAL impl and remove sensors.msm8996
    All the sensors stuff is handled in HIDL now
    
    Change-Id: I4bfafaf553292beabe4ccb23e02de12d3e5261a5
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- device.mk
+++ device.mk
@@ -308,7 +308,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.msm8996
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 # Wifi
 PRODUCT_PACKAGES += \
