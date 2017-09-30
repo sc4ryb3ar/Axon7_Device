7b98209effb456edcbe97891a9bb89dd1fc5cf98
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 13:06:58 -0400

    axon7: Built libbt-vendor
    Change-Id: I1ef68c289377c04429226522b82344809e52f441
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- device.mk
+++ device.mk
@@ -146,7 +146,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
 
 # Charger
 PRODUCT_PACKAGES += \
