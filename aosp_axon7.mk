51e2a0daa06e7cd2fb1c08b14cfca3b0b24d90e8
Paul Ravenstone <sc4ryb3ar@gmail.com>
2017-11-11 22:07:13 -0500

    correction for pure fusion specs

--- aosp_axon7.mk
+++ aosp_axon7.mk
@@ -2,7 +2,7 @@
 PRODUCT_COPY_FILES := device/zte/axon7/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Include pure telephony configuration
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/pure/configs/pure_phone.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
