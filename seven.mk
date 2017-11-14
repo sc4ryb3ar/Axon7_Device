538c9a49841469d7288a2ae75ef26d441ed4c52b
Paul Ravenstone <sc4ryb3ar@gmail.com>
2017-11-13 23:03:06 -0500

    moving things around

--- seven.mk
+++ seven.mk
@@ -1,18 +1,16 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 Pure Fusion OS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -233,7 +231,7 @@ PRODUCT_PACKAGES += \
 
 # Recovery
 PRODUCT_PACKAGES += \
-    librecovery_updater_axon7 
+    librecovery_updater_axon7
 
 # RIL
 PRODUCT_PACKAGES += \
